67f25d27fd7aaf6536572d20aa7e01e650a72160
Nishok-Sasikumar <nishok2005kuro@gmail.com>
2026-07-03 20:15:18 +0530

    Ronaldo better

--- datatypes.py
+++ datatypes.py
@@ -2,7 +2,7 @@ a = 1
 b = 2.2
 c  = "String"
 logged = True
-print( "the type of a is " ,type(a) , a)
-print( "the type of b is " ,type(b) , b)
-print( "the type of c is " ,type(c) , c)
-print( "the type of logged is " ,type(logged) , logged)
+print(type(a) , a)
+print(type(b) , b)
+print(type(c) , c)
+print(type(logged) , logged)
